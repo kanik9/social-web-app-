a98451715b9c3beb1a66c6bc36321600ac7b28c4
Kanik vijay <32274469+kanik9@users.noreply.github.com>
2021-05-06 18:43:16 +0530

    Update urls.py

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from . import settings
 from . import views
 
 urlpatterns = [
@@ -26,4 +28,4 @@ urlpatterns = [
     url(r"^accounts/", include("django.contrib.auth.urls")),
     url(r"^posts/", include("posts.urls", namespace="posts")),
     url(r"^groups/", include("groups.urls", namespace="groups")),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
